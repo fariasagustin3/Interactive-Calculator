39702f8ca646dc95623a90cce3221d68fb06960f
fariasagustin3 <fariasagustin3@gmail.com>
2022-03-22 16:49:07 -0300

    initial commit

---
+++ src/components/Boton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../stylesheets/Boton.css';
+
+function Boton(props) {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor != '.') && (valor != '=');
+  };
+
+  return (
+    <div
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`}
+      onClick={() => props.manejarClic(props.children)}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Boton;
